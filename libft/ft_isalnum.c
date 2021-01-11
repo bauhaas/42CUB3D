e1e06a63a71e2afc530ea858776a63e6f7d60de3
bahaas <baudoin.haas@gmail.com>
2021-01-11 12:05:17 +0100

    Add: minilibx & libft dir

--- libft/ft_isalnum.c
+++ libft/ft_isalnum.c
@@ -1,26 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bahaas <bahaas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/12 23:03:02 by bahaas            #+#    #+#             */
-/*   Updated: 2020/11/25 18:17:30 by bahaas           ###   ########.fr       */
+/*   Created: 2020/09/16 22:52:07 by bahaas            #+#    #+#             */
+/*   Updated: 2020/10/13 21:32:59 by bahaas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "libft.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s);
-char	*free_then_join(char *s1, char *s2, int size_s1);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-int		get_next_line(int fd, char **line);
-
-#endif
+int			ft_isalnum(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+			|| (c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
---
+++ libft/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bahaas <bahaas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 23:03:02 by bahaas            #+#    #+#             */
+/*   Updated: 2021/01/11 12:02:22 by bahaas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+#endif
+
+# include <unistd.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+char	*free_then_join(char *s1, char *s2, int size_s1);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+int		get_next_line(int fd, char **line);
+
+#endif
